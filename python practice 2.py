1ca574e5fb1ce83271bb6aa0c5cba4b1441628d5
jmohammedyaseen <jmohammedyaseen2@gmail.com>
2026-07-29 19:04:30 +0530

    added python practice:armstrong number

--- python practice 2.py
+++ python practice 2.py
@@ -1,2 +1,28 @@
-a = input("enter the name: ")
-print("Hello, " + a)
+num = int(input("emter the number"))
+
+orgi = num
+orgi2 = num
+count = 0 
+sum2 = 0
+
+while num>0:
+    num = num // 10
+    count += 1
+
+# while num>0:
+#     digit = num%10
+#     sum+=digit
+
+while orgi>0:
+    digi = orgi%10
+    sum2 +=digi**count
+    orgi= orgi//10
+
+
+if orgi2 == sum2:
+    print(orgi2,"is a armstrong number")
+
+else:
+    print("not a armstrong number")
+
+print(sum2)
