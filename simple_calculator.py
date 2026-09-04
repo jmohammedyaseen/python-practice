a4ec99115f7a828c62b74e84cd3e2e069d1b036e
jmohammedyaseen <jmohammedyaseen2@gmail.com>
2026-09-04 15:46:04 +0530

    add simple_calculator program

---
+++ simple_calculator.py
@@ -0,0 +1,30 @@
+operator  = input("enter the operation +,-,*,/ : ")
+
+number1 = float(input("enter the number"))
+
+number2 = float(input("enrter the number"))
+
+if operator == "+":
+    result = number1 + number2 
+    print(result)
+
+elif operator == "-":
+    result = number1 + number2 
+    print(round(result,3))
+
+elif operator == "*":
+    result = number1 * number2
+    print(round(result,3))
+
+elif operator == "/":
+    result = number1 / number2 
+    print(round(result,3))
+
+else :
+    print(" the",operator,"is not valid")
+
+
+
+
+
+
